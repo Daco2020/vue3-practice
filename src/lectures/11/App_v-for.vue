a2d670022b9c3e81b0eae74d43aa232ab0507ea0
Daco2020 <dacokim32@gmail.com>
2023-01-09 19:15:26 +0900

    lectur11

---
+++ src/lectures/11/App_v-for.vue
@@ -0,0 +1,42 @@
+<template>
+	<div>
+		<ul>
+			<template v-for="(item, index) in evenItems" :key="item.id">
+				<!-- <li v-if="item.id % 2 === 0"></li> -->
+				<li>ID: {{ item.id }} 인덱스: {{ index }}, {{ item.message }}</li>
+			</template>
+		</ul>
+		<hr />
+		<ul>
+			<li v-for="(value, key, index) in myObject" :key="key">
+				{{ index }}-{{ key }}-{{ value }}
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+import { computed, reactive } from 'vue';
+
+export default {
+	setup() {
+		const items = reactive([
+			{ id: 1, message: 'Jave' },
+			{ id: 2, message: 'HTML' },
+			{ id: 3, message: 'CSS' },
+			{ id: 4, message: 'Python' },
+		]);
+
+		const evenItems = computed(() => items.filter(item => item.id % 2 === 0));
+
+		const myObject = reactive({
+			title: '제목',
+			author: '다코',
+			publishedAt: '2022-12-26',
+		});
+		return { items, evenItems, myObject };
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
