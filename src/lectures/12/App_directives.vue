563b9058ba270026b7cfceb93e6da9f23f02fb49
Daco2020 <dacokim32@gmail.com>
2023-01-11 16:06:27 +0900

    lectur12

---
+++ src/lectures/12/App_directives.vue
@@ -0,0 +1,51 @@
+<template>
+	<div>
+		<!-- 안녕하세요 -->
+		<p>{{ msg }}</p>
+		<!-- 안녕하세요 -->
+		<p v-text="msg"></p>
+		<!-- **뀨!** -->
+		<p v-html="htmlStr"></p>
+		<!-- <strong>뀨!</strong> -->
+		<p v-text="htmlStr"></p>
+		<!-- 표현식을 허용하지 않음 그대로 보여줌 -->
+		<!-- {{ msg 안녕하세요}} -->
+		<p v-pre>{{ msg 안녕하세요}}</p>
+		<!-- 안녕하세요 -->
+		<!-- 이후 변경되지 않음. 업데이트 성능최적화 -->
+		<p v-once>{{ msg }}</p>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+	setup() {
+		const msg = ref('안녕하세요');
+		const htmlStr = ref('<strong>뀨!</strong>');
+		return { msg, htmlStr };
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
+
+<!-- 
+
+// v-cloak 컴파일 되기 전까지 v-cloak 선택자를 지정해 스타일을 바꿔줄 수 있다.
+// 안보이게 하거나 로딩중을 표현할 수 있다.
+
+<style>
+[v-cloak] {
+	display: none;
+}
+</style>
+
+<template>
+<div v-cloak>
+  {{ message }}
+</div> 
+</template>
+
+-->
---
+++ src/lectures/12/App_v-memo.vue
@@ -0,0 +1,33 @@
+<template>
+	<!-- memo 배열 안에 있는 요소만 반응한다 -->
+	<!-- 성능최적화에 사용 -->
+	<div v-memo="[subscribers]">
+		<p>a : {{ subscribers }}</p>
+		<p>b : {{ views }}</p>
+		<p>c : {{ likes }}</p>
+	</div>
+	<!-- v-on: -> @ 로 단축할 수 있음 -->
+	<button @click="subscribers++">sub++</button>
+	<button @click="views++">view++</button>
+	<button @click="likes++">like++</button>
+	<div>
+		<p>a : {{ subscribers }}</p>
+		<p>b : {{ views }}</p>
+		<p>c : {{ likes }}</p>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+	setup() {
+		const subscribers = ref(4000);
+		const views = ref(400);
+		const likes = ref(20);
+		return { subscribers, views, likes };
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
